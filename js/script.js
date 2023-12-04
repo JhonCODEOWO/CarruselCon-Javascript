5b45eae6d4c3af3980117b6a4f8aed09a29ac05a
Jhon <jjv20618@gmail.com>
2023-12-04 09:17:49 -0600

    Animaciones agregadas.

--- js/script.js
+++ js/script.js
@@ -2,23 +2,65 @@
 const btnAnterior = document.querySelector('#previous-image');
 const btnSiguiente = document.querySelector('#next-image');
 const divImage = document.querySelector('#imagen-carrusel');
+const divTituloCarrusel = document.querySelector('#titulo-carrusel');
+const divEnlaceCarrusel = document.querySelector('#hipervinculo-carrusel');
 //-----------------------------------------------------------
 
 //Arreglo con las direcciones de las imágenes
 const imagenes = [
-    "img/imagen1.png",
-    "img/imagen2.png",
-    "img/imagen3.png"
+    "img/imagen1.png_Prueba titulo_https://www.itssnp.edu.mx/index.php",
+    "img/imagen2.png_Prueba titulo",
+    "img/imagen3.png_Prueba titulo_https://www.itssnp.edu.mx/index.php"
 ]
 
 //indice para la navegación en el arreglo
 let indice = 0;
 
-
+document.addEventListener("DOMContentLoaded", function(){
+    cambiarConTiempo();
+});
 
 //Método que aplica la imágen en base al índice recibido
 function aplicarIndice(indice) {
-    divImage.style.backgroundImage = "url(" + imagenes[indice] + ")";
+    let subDivision = imagenes[indice].split('_');
+    let ligaImg = subDivision[0];
+    let tituloImg = subDivision[1];
+    let hipervinvulo = subDivision[2];
+    console.log(`URL: ${ligaImg} Titulo de imagen: ${tituloImg} Hipervinculo: ${hipervinvulo}` );
+    //Colocar titulo
+    divTituloCarrusel.textContent = tituloImg;
+    //Colocar hipervinculo si existe
+    if (hipervinvulo != null) {
+        divEnlaceCarrusel.style.display = "block";
+        divEnlaceCarrusel.textContent = "Conoce más >"
+        divEnlaceCarrusel.href = hipervinvulo
+    }else{
+        divEnlaceCarrusel.style.display = "none";
+    }
+    //Colocar imagen correspondiente
+    divImage.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)) , url(" + ligaImg + ")";
+}
+
+
+
+function cambiarConTiempo(){
+    setInterval( () => {
+
+        console.log("Se ha entrado en el timeout");
+        console.log(`Indice actual: ${indice} - Indice máximo: ${imagenes.length}`);
+
+        if (indice == imagenes.length - 1) {
+
+            console.log("Se ha alcanzado el máximo del arreglo");
+
+            indice = 0;
+            aplicarIndice(indice);
+        }else{
+            indice++;
+            console.log(`Indice actual dentro del timeinterval ${indice} -> ${imagenes[indice]}`);
+            aplicarIndice(indice);
+        }
+    } , 2000);
 }
 
 btnAnterior.addEventListener('click', function(){
@@ -33,8 +75,11 @@ btnAnterior.addEventListener('click', function(){
 });
 
 btnSiguiente.addEventListener('click', function(){
-    if (indice == 2) {
-        alert("Has llegado al límite máximo de los elementos");
+    console.log(`Indice actual dentro del listener siguiente ${indice}`);
+    if (indice == imagenes.length - 1) {
+        console.log("Has llegado al límite máximo de los elementos");
+        indice = 0;
+        aplicarIndice(indice);
     } else {
         indice++;
         console.log(imagenes[indice]);
